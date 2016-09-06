ef3e78caf53f781d5c738170d2ea5a76f103a1cf
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-09-06 15:40:51 +0530

    adding pnp.

--- src/user-custom-actions/say-hello/say-hello.js
+++ src/user-custom-actions/say-hello/say-hello.js
@@ -1,6 +1,10 @@
 import MDS from '../../MDS';
+import pnp from 'sp-pnp-js';
 
 MDS('http://localhost:7777/scripts/say-hello.bundle.js', () => {
   console.log('Hello from say-hello!');
+  pnp.sp.web.select('Title').get().then((data) => {
+    console.log(data);
+  });
   // SP.SOD.notifyScriptLoadedAndExecuteWaitingJobs('say-hello.js');
 });
